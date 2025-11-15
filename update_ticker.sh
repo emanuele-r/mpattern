fb5c3768d9838565797f206df71173aca70610c0
lehman <m.reds1@icloud.com>
2025-11-15 12:49:31 +0100

    added stocks to ticker_list

--- update_ticker.sh
+++ update_ticker.sh
@@ -1,6 +1,5 @@
 curl -s https://www.nasdaqtrader.com/dynamic/symdir/nasdaqlisted.txt | \
 awk -F'|' 'NR>1 && $1 !~ /^File Creation Time/ {print $1}' | \
-head -20 | \
 while read ticker; do 
-    echo "insert or replace into symbols (ticker) values ('$ticker');"
+    echo "insert or replace into ticker_list (ticker, category, change, close) values ('$ticker', 'stock', '0' , '0');"
 done | sqlite3 asset_prices.db
